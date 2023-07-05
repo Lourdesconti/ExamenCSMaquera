e4fa65fb1008c3628588234db2b1bf1eda21c4cf
LOURDESMAQCONTI <72649170@continental.edu.pe>
2023-07-05 15:04:22 -0500

    Primera Parte

---
+++ src/main/java/ch/makery/address/util/LocalDateAdapter.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch.makery.address.util;
+
+
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import java.time.LocalDate;
+
+import java.time.LocalDate;
+
+/**
+ * Adapter (for JAXB) to convert between the LocalDate and the ISO 8601 
+ * String representation of the date such as '2012-12-03'.
+ * 
+ * @author Marco Jakob
+ */
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+	@Override
+	public LocalDate unmarshal(String v) throws Exception {
+		return LocalDate.parse(v);
+	}
+
+	@Override
+	public String marshal(LocalDate v) throws Exception {
+		return v.toString();
+	}
+}
